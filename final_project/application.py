a8da96cb66a41fec26ff210b0301ed9c37cccd80
andrayantelo <andrayantelo@gmail.com>
2019-02-11 20:53:08 +0000

    added js, css, and html files

--- final_project/application.py
+++ final_project/application.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
-import SQL
+import sqlite3
 
 # Configure application
 app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    return 'Home page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, world'
 
---
+++ final_project/static/scripts.js
@@ -0,0 +1,1 @@
+let mymap = L.map('map').setView([30.2672, 97.7431], 13)

59b06f9daf94e4e72159fd18e258f2713627a572
andrayantelo <andrayantelo@gmail.com>
2019-03-12 01:44:47 +0000

    wrote two unit tests, one for isLat, one for isLong

--- final_project/application.py
+++ final_project/application.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 def home_page(name=None):
     return render_template('index.html', name=name)
     
+@app.route('/tests')
+def test_page(name=None):
+    return render_template('tests.html', name=name)
+    
 
--- final_project/static/qunit.js
+++ final_project/static/qunit.js
@@ -1,1 +1,42 @@
 // Qunit JS file
+QUnit.begin(function( details ) {
+  console.log( "Test amount:", details.totalTests );
+  $('#qunit-fixture').append('<div id="mapid"></div>');
+});
+
+QUnit.module("helper functions tests");
+
+QUnit.test("latitudinal value test", function(assert) {
+    assert.expect(11);
+    assert.ok(isLat(-90));
+    assert.notOk(isLat(-180));
+    assert.notOk(isLat(180));
+    assert.ok(isLat(90));
+    assert.ok(isLat(0));
+    // test non numerical values
+    assert.notOk(isLat('hello'));
+    assert.ok(isLat('0'));
+    assert.notOk(isLat(true));
+    assert.notOk(isLat(NaN));
+    assert.notOk(isLat(undefined));
+    assert.notOk(isLat(null));
+    
+});
+
+QUnit.test("longitudinal value test", function(assert) {
+    assert.expect(11);
+    assert.ok(isLong(180));
+    assert.ok(isLong(-180));
+    assert.ok(isLong(0));
+    assert.notOk(isLong(190));
+    assert.notOk(isLong(-190));
+    // test non numerical values
+    assert.notOk(isLong('hello'));
+    assert.ok(isLong('0'));
+    assert.notOk(isLong(true));
+    assert.notOk(isLong(NaN));
+    assert.notOk(isLong(undefined));
+    assert.notOk(isLong(null));
+    
+});
+

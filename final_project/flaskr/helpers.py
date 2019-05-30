6bfe9a295e608b4edcd2a88496fb62c7c124957d
andrayantelo <andrayantelo@gmail.com>
2019-05-30 22:25:41 +0000

    wrote a helper function that validates the coordinates that were input

--- final_project/flaskr/helpers.py
+++ final_project/flaskr/helpers.py
@@ -1,6 +1,45 @@
 # Helper functions
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
 
+class RangeError(Error):
+    """Exception raised for value being out of a particular range.
+    
+    Attributes:
+        expression -- input expression in which the error occured
+        message -- explanation of the error
+    """
+    
+    def __init__(self, value, message):
+        self.value = value
+        self.message = message
+        
+    def __str__(self):
+        return(repr(self.value))
+        
+    
 
 # function that validates coordinate data
-def validate_coordinates():
-    pass
+def validate_coordinates(coordinates):
+    # verify that lat and lng are floats
+    lat, lng = coordinates
+    try:
+        # verify they are floats
+        float(lat)
+        float(lng)
+        # verify that they fall within their ranges
+        if lat < -90 and lat > 90:
+            #print("{} not in range.".format(lat))
+            raise RangeError(lat, "lat not in range")
+        if lng < -180 and lng > 180:
+            #print("{} not in range.".format(lng))
+            raise RangeError(lng, "long not in range")
+        
+    except ValueError:
+        return False
+    except RangeError as e:
+        print(e.message)
+        
+    # return true if all tests passed
+    return True

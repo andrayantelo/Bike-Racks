34f36bcd2ceb60e926235d96000704dc21f2ad8c
John Tyree <johntyree@gmail.com>
2019-09-29 09:37:10 +0100

    Add real setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# coding: utf8
 
-    
+from setuptools import setup
 
+setup(name='bikeracks',
+      version='0.0.1',
+      author='Andrea Anaya',
+      url='http://github.com/andrayantelo/cs50-final-project',
+      description="Bikerack quality and location",
+      py_modules=['bikeracks'],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          ],
+      )

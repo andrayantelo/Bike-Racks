81fd6d7c89d7b4041d83e8933050c75f44e76bbd
andrayantelo <andrayantelo@gmail.com>
2020-03-24 11:41:41 -0700

    Include functions for notifying success/error to user for submitting a bike rack removal suggestion and for submitting feedback.

--- src/bikeracks/static/notification.js
+++ src/bikeracks/static/notification.js
@@ -1,5 +1,5 @@
 const Notifications = (() => {
-    return {
+    const exports = {
         // Options and settings for alerts
         // Success/error messages for submitting a bike rack removal suggestion
         suggestionErrorMessage: "Sorry, unable to send suggestion at this time.",
@@ -15,15 +15,52 @@ const Notifications = (() => {
         submitBikeError: "Unable to add bike rack at this time. Please try again later.",
         errorIcon: 'glyphicon glyphicon-warning-sign',
         successIcon: 'glyphicon glyphicon-ok',
-        renderMessage: (icon, message) => {
-            return {
-                icon,
-                message
-            }
-        },
         errorSettings: {type: "danger"},
         successSettings: {type: "success"}
     }
+    exports.renderMessage = (icon, message) => {
+        return {
+            icon,
+            message
+        }
+    }
+    exports.notifyFeedbackSuccess = () => {
+        $.notify(
+            exports.renderMessage(
+                exports.successIcon,
+                exports.feedbackSuccessMessage
+            ),
+            exports.successSettings
+        )
+    }
+    exports.notifyFeedbackError = () => {
+        $.notify(
+            exports.renderMessage(
+                exports.errorIcon,
+                exports.feedbackErrorMessage
+            ),
+            exports.errorSettings
+        )
+    }
+    exports.notifySubmitRemovalSuccess = () => {
+        $.notify(
+            exports.renderMessage(
+                exports.successIcon,
+                exports.suggestionSuccessMessage
+            ),
+            exports.successSettings
+        )
+    }
+    exports.notifySubmitRemovalError = () => {
+        $.notify(
+            exports.renderMessage(
+                exports.errorIcon,
+                exports.suggestionErrorMessage
+            ),
+            exports.errorSettings
+        )
+    }
+    return exports;
 })()
 
 
